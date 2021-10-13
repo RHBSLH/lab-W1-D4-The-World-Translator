96f93ece927917b3382d0c24d0bfc8458f1c4a95
RHBSLH <Rehab.Saleh.abt.js@tuwaiq.edu.sa>
2021-10-13 11:59:54 +0300

    lab w1 d4

---
+++ worldlanguage.js
@@ -0,0 +1,13 @@
+let language = "English"
+
+if (language === "English") {
+    console.log("hello world")
+
+}else if (language === "Arabic"){
+console.log("مرحبا بالعالم")
+
+}else if (language === "Frensh"){
+    console.log("ponjor")
+}else {
+    console.log("not find language")
+}
